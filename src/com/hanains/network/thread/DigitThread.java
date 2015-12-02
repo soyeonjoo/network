06a35bb9915232e48420eab6dfad2e3759c78526
kickscar <kickscar@gmail.com>
2015-12-02 16:44:34 +0900

    warning 없애기 ㅋ

--- src/com/hanains/network/thread/DigitThread.java
+++ src/com/hanains/network/thread/DigitThread.java
@@ -1,11 +1,6 @@
 package com.hanains.network.thread;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class DigitThread extends Thread {
-
-	
 	@Override
 	public void run() {
 		for( int i = 0; i < 10; i++ ) {
