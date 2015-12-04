2150576e742dd2f533932495cdcc4334a7d34081
bit-user <bit-user@bit>
2015-12-04 10:47:22 +0900

    UDP Echo Server

---
+++ src/com/hanains/network/echo/UDPEchoClient.java
@@ -0,0 +1,52 @@
+package com.hanains.network.echo;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+
+public class UDPEchoClient {
+	
+	private static String HOST_ADDRESS = "127.0.0.1";
+	private static int PORT = 9090;
+	private static int BUFFER_SIZE = 1024;
+	
+	public static void main(String[] args) {
+		
+		DatagramSocket datagramSocket = null;
+		
+		try {
+
+			// 1. UDP 소켓 생성
+			datagramSocket = new DatagramSocket();
+			
+			//2. 전송 패킷 생성
+			String data = "Hello World";
+			byte[] sendData = data.getBytes( "UTF-8" );
+			DatagramPacket sendPacket
+				= new DatagramPacket( sendData, sendData.length,  new InetSocketAddress( HOST_ADDRESS, PORT ) );
+
+			//3. 데이터 전송
+			datagramSocket.send( sendPacket );
+			
+			//4. 데이터 수신
+			DatagramPacket receivePacket = new DatagramPacket( new byte[ BUFFER_SIZE], BUFFER_SIZE );
+			datagramSocket.receive( receivePacket );
+			
+			//5. 데이터 출력
+			data = new String( receivePacket.getData(), 0, receivePacket.getLength(), "UTF-8" );
+			log( "데이터 수신 " + data  );
+			
+		} catch( Exception ex ) {
+			log( "error:" + ex);
+		} finally {
+			if( datagramSocket != null ) {
+				datagramSocket.close();
+			}
+		}
+	}
+	
+	public static void log( String message ) {
+		System.out.println( "[UDP Echo Client]" +  message );
+	}
+
+}
---
+++ src/com/hanains/network/echo/UDPEchoServer.java
@@ -0,0 +1,50 @@
+package com.hanains.network.echo;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+
+public class UDPEchoServer {
+
+	private static final int PORT = 9090;
+	private static final int BUFFER_SIZE = 1024;
+	
+	public static void main(String[] args) {
+		
+		DatagramSocket datagramSocket = null;
+		
+		try {
+			//1. UDP 소켓 생성
+			datagramSocket = new DatagramSocket( PORT );
+			
+			//2. 수신 대기
+			log( "수신 대기" );
+			DatagramPacket receivePacket = new DatagramPacket( new byte[ BUFFER_SIZE], BUFFER_SIZE );
+			datagramSocket.receive( receivePacket );
+			
+			//3. 데이터 확인
+			String data = new String( receivePacket.getData(), 0, receivePacket.getLength(), "UTF-8" );
+			log( "데이터 수신:" + data );
+			
+			//4. 데이터 전송
+			DatagramPacket sendPacket
+				= new DatagramPacket(
+						receivePacket.getData(), 
+						receivePacket.getLength(),
+						receivePacket.getAddress(),
+						receivePacket.getPort() );
+			datagramSocket.send( sendPacket );
+			
+		} catch( Exception ex ) {
+			log( "error:" + ex );
+		} finally {
+			if( datagramSocket != null ) {
+				datagramSocket.close();
+			}
+		}
+	}
+
+	public static void log( String message ) {
+		System.out.println( "[UDP Echo Server] " + message );
+	}
+}
